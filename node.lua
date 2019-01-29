c3c56e55f5cda81deceb2a751ec0f36d3739bd4a
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-01-29 15:14:37 +0100

    Create node.lua

---
+++ node.lua
@@ -0,0 +1,17 @@
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
+local font = resource.load_font "ARIALN.TTF"
+local text = "Hello World"
+
+util.json_watch("config.json", function(config)
+    -- called each time the configuration is changed. 'config'
+    -- contains new current configuration values set by the user.
+
+    text = config.text -- assign configuration 'text' value to 'text' variable
+end)
+
+function node.render()
+    gl.clear(0,0,0,1)
+    time = os.now
+    font:write(250, 300, time, 64, 1,1,1,1) 
+end
