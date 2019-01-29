6f3de8671a927f8b22a39268d682a3687938044c
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-01-29 17:14:15 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -2,13 +2,10 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font "ARIALN.TTF"
 local text = "Hello World"
-local t_delta1 
-local t_delta2
-local my_time1
-local my_time2
+local t_delta 
+local my_time = sys.now()
+local my_size = 100
 
-my_time1 = os.time()
-my_time2 = sys.now()
 
 util.json_watch("config.json", function(config)
     -- called each time the configuration is changed. 'config'
@@ -19,14 +16,10 @@ end)
 
 function node.render()
     gl.clear(0,0,0,1)
-    t_delta1 = os.time() - my_time1
-    t_delta2 = sys.now() - my_time2
-    my_time1 = os.time()
-    my_time2 = sys.now()
-
+    t_delta = sys.now() - my_time
+    my_time = sys.now()
+    my_size = my_size +(t_delta * 25)
     font:write(0, 0, "Size Test", 32, 1,1,1,1) 
-    my_time1 = os.time()
-    my_time2 = sys.now()
-    font:write(250, 300, "Os.time : "..my_time1.."  "..t_delta1 , 32, 1,1,1,1) 
-    font:write(250, 400, "Sys.now : "..my_time2.."  "..t_delta2 , 32, 1,1,1,1) 
+    font:write(0, 200, "Sys.now : "..my_time.."  "..t_delta.."  "..my_size , 64, 1,1,1,1)
+    font:write(0, 300, "Sys.now : "X" , my_size, 1,1,1,1)
 end
