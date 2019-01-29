2f675747b8bef88206a8e514a25147fbb5ff284d
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-01-29 16:47:46 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -2,6 +2,13 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font "ARIALN.TTF"
 local text = "Hello World"
+local t_delta1 
+local t_delta2
+local my_time1
+local my_time2
+
+my_time1 = os.time()
+my_time2 = sys.now()
 
 util.json_watch("config.json", function(config)
     -- called each time the configuration is changed. 'config'
@@ -12,9 +19,14 @@ end)
 
 function node.render()
     gl.clear(0,0,0,1)
-    font:write(0, 0, "Size Test", 64, 1,1,1,1) 
+    t_delta1 = os.time() - my_time1
+    t_delta2 = sys.now() - my_time2
     my_time1 = os.time()
     my_time2 = sys.now()
-    font:write(250, 300, "Os.time : "..my_time1, 64, 1,1,1,1) 
-    font:write(250, 400, "Sys.now : "..my_time2, 64, 1,1,1,1) 
+
+    font:write(0, 0, "Size Test", 32, 1,1,1,1) 
+    my_time1 = os.time()
+    my_time2 = sys.now()
+    font:write(250, 300, "Os.time : "..my_time1.."  "..t_delta1 , 32, 1,1,1,1) 
+    font:write(250, 400, "Sys.now : "..my_time2.."  "..t_delta2 , 32, 1,1,1,1) 
 end
