1b3118f8c3330a6fcab6d697f2ae651a5b67f40e
john-njmj <46443771+john-njmj@users.noreply.github.com>
2019-02-03 06:51:59 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -18,11 +18,13 @@ function node.render()
     gl.clear(0,0,0,1)
     t_delta = sys.now() - my_time
     my_time = sys.now()
-    my_size = my_size +(t_delta * 25)
+    --my_size = my_size +(t_delta * 25)
     font:write(0, 0, "Size Test", 32, 1,1,1,1) 
     font:write(0, 100, "Sys.now : "..my_time.."  "..t_delta.."  "..my_size , 45, 1,1,1,1)
     write_w = font:write(0, 300, "X" , my_size, 1,1,1,1)
     font:write(0, 150, "Write width : "..write_w.."  "..my_size , 45, 1,1,1,1)
     calc_w = font:width(0, 300, "X" , my_size)
     font:write(0, 200, "calc width : "..calc_w.."  "..my_size , 45, 1,1,1,1)
+    my_text = "text" .. \t .."text"
+    font:write(0, 500, my_text, 32, 1,1,1,1)
 end
